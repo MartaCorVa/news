741c48458d0c4799e9aa4cb59081799ec7d005b2
Marta <martacorcolesvalle@gmail.com>
2022-07-16 18:19:47 +0200

    Functions for archive a new and delete a new implemented.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -20,3 +20,16 @@ export const setActualPage =  ( state, page ) => {
 
 }
 
+export const deleteNew =  (state, id) => {
+
+    state.archivedNews = state.archivedNews.filter( t => t._id != id )
+
+}
+
+export const archiveNew =  (state, id) => {
+
+    state.archivedNews = [ ...state.archivedNews, state.news.find( t => t._id == id ) ] 
+    state.news = state.news.filter( t => t._id != id )
+
+}
+
