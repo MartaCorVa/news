dd723a6a44cf46a9e5071a1b34193bc5dd5a2c53
Marta <martacorcolesvalle@gmail.com>
2022-07-16 12:57:36 +0200

    Methods get archived news and get actual page created.

--- src/store/getters.js
+++ src/store/getters.js
@@ -6,3 +6,15 @@ export const getNews = ( state ) => {
 
 }
 
+export const getArchivedNews = ( state ) => {
+
+    return state.archivedNews
+
+}
+
+export const getActualPage = ( state ) => {
+
+    return state.actualPage
+
+}
+
